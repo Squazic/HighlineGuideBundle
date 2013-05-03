58849dfa4552cdf920545a2da8fe64df54a04a9e
Steven Chen <szchen77@gmail.com>
2013-05-03 01:32:09 -0700

    Add route for data feed and json response

--- src/Squazic/HighlineGuideBundle/Controller/DefaultController.php
+++ src/Squazic/HighlineGuideBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Squazic\HighlineGuideBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -12,6 +13,19 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('SquazicHighlineGuideBundle:Default:index.html.twig', array('name' => 'Test'));
+    }
+
+    public function dataAction()
+    {
+        $conn = $this->get('database_connection');
+
+        $sql = 'SELECT w.title, w.description, w.latitude, w.longitude, a.name FROM artwork w
+                JOIN artist_artwork aa ON w.id = aa.artwork_id
+                JOIN artist a ON aa.artist_id = a.id';
+
+        $data = $conn->fetchAll($sql);
+
+        return new JsonResponse($data);
     }
 
     /**
